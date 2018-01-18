76bb919bab590e547e66bbdbf168420d37fb0e36
Patnayak, Nilikh <Nilikh.Patnayak@searshc.com>
2018-01-18 14:14:59 -0600

    Some changes

--- ContactLBTA/ViewController.swift
+++ ContactLBTA/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let cellId = "cellId"
+    
+    let names = [
+        "Amy", "Steve", "Jack", "Bill", "Mark"
+    ]
+    
+    let twoDimensionalArray = [
+        ["Amy", "Steve", "Jack", "Bill", "Mark"],
+        ["qw", "qwr"],
+        ["sfs", "sdff", "erer", "tertr"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = UIColor.lightGray
+        return label
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionalArray.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimensionalArray[section].count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let name = twoDimensionalArray[indexPath.section][indexPath.row]
+        cell.textLabel?.text = "\(name) section: \(indexPath.section) row: \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
